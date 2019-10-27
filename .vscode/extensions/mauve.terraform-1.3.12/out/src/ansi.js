670155d6e438098b1721e946bbd2745b2562bb4e
Glenn Gillen <me@glenngillen.com>
2019-10-27 11:08:41 +1100

    Add rust/cargo to path.

--- .vscode/extensions/mauve.terraform-1.3.12/out/src/ansi.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const ansiRegex = /[\u001b\u009b][[()#;?]*(?:[0-9]{1,4}(?:;[0-9]{0,4})*)?[0-9A-ORZcf-nqry=><]/g;
-function stripAnsi(text) {
-    return text.replace(ansiRegex, '');
-}
-exports.stripAnsi = stripAnsi;
-
-//# sourceMappingURL=ansi.js.map
--- .vscode/extensions/rebornix.ruby-0.24.1/server/out/providers/Provider.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Provider {
-    constructor(connection) {
-        this.connection = connection;
-    }
-}
-exports.default = Provider;
-//# sourceMappingURL=Provider.js.map
